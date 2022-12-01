7e06a21cd7dbde8cade24e376c6e0757e51aed48
Josh Lengel <jlengel12@gmail.com>
2022-12-01 02:05:33 +0100

    Added hostname and port as arguments

--- example/main.c
+++ example/main.c
@@ -1,15 +1,26 @@
 #include<cserv/cserv.h>
 
 #include<stdio.h>
+#include<stdlib.h>
 
 static void handler(int fd)
 {
     printf("Client connected...\n");
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    cserver *serv = cserver_init("0.0.0.0", 8080, 0);
+    if (argc != 3)
+    {
+        printf("Usage: cserv_example hostname port\n");
+        return 1;
+    }
+
+    const char *hostname = argv[1];
+    const char *port_str = argv[2];
+    uint16_t port = (uint16_t)strtoul(port_str, NULL, 10);
+
+    cserver *serv = cserver_init(hostname, port, 0);
     if (!serv)
         return 1;
     
